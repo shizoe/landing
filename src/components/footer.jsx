f942defd2d69b9ea18a53d3b19a30ac10f4eecd4
Mabo Mbebeta <mabombebeta@my.uopeople.edu>
2023-01-05 15:23:32 +0200

    First Commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = (props) => {
+  return (
+    <div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; 2022 mLINKME Landing Page. Designed by{" "}
+            <a href="http://www.mabombebeta.ml" rel="nofollow">
+              Mabo Mbebeta
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
